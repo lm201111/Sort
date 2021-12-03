079f9373250e29af9162fbb4d503a00a1fefbee2
lm201111 <lm201111@mail.ru>
2021-12-03 13:46:23 +0400

    Commit message.

---
+++ server/src/main/java/com/example/server/bean/SortType.java
@@ -0,0 +1,10 @@
+package com.example.server.bean;
+
+
+public enum SortType {
+    BUBBLE_SORT,
+    QUICK_SORT,
+    INSERTION_SORT,
+    HEAP_SORT,
+    SELECTION_SORT
+}
---
+++ server/src/main/java/com/example/server/service/BubbleSortService.java
@@ -0,0 +1,30 @@
+package com.example.server.service;
+
+import com.example.server.bean.Line;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BubbleSortService extends SortService{
+    @Override
+    protected List<Line> sortList(String target, List<Line> list) {
+        boolean found = true;
+        while (found){
+            found = false;
+            for(int i =0; i<list.size() - 1; i++){
+                Line current = list.get(i);
+                Line next = list.get(i + 1);
+                if(current.needToReplace(next)){
+                    found = true;
+                    list.set(i, next);
+                    list.set(i+1, current);
+                    addLog(target, i, list);
+                }
+            }
+        }
+        return list;
+    }
+
+
+}
